2c40748b3fe65e6e0362c6ab284b7a47f382e20d
JoseGuzman02 <83370645+JhonGuzmanS@users.noreply.github.com>
2023-12-11 10:08:17 -0500

    updated streamlit and the dataset

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -29,6 +29,7 @@ def load_data(DATA, nrows):
 data_load_state = st.text('Loading data...')
 raw_data = load_data(RAW_DATA_URL, 20000)  # dataset has ~20000
 upset_data = load_data(UPSET_DATA, 10000)
+upset_data = upset_data.drop(columns=["index", "unnamed: 0"])
 
 # displays 2 datasets
 if st.checkbox('Show raw chess data'):
@@ -49,52 +50,50 @@ st.subheader("Let's look at a random match!")
 st.button('Click me', on_click=get_rand_match)
 st.write("Note: result is generated on the top of the screen")
 
-#
+# Searches a player
 players_upset = pd.concat([upset_data['white_id'], upset_data['black_id']]).drop_duplicates()
 st.subheader("Below are the usernames that are within the dataset. \n Specifically the 'interesting' players")
 st.write(players_upset)
 
-st.subheader("Search a Player!")
-user_input = st.text_input("Enter in a number ranging from 0 - 545 or a player_id to look for a match:")
+st.subheader("Search a match!")
+user_input = st.text_input("Enter in a number ranging from 0 - 545 or a player_id:")
 if user_input:
     st.subheader('Match Information')
-    st.text(f"You entered: {user_input}. Let's see the matche(s)!")
+
     try:
         match_history = upset_data.iloc[[int(user_input)]]
+        st.text(f"Let's see match {user_input}!")
         st.write(match_history)
         st.write(match_history["moves"])
 
     except:
+        st.text(f"Let's see '{user_input}' match history!")
         result = upset_data.query(f'((white_id == "{user_input}") or (black_id == "{user_input}"))')
         st.write(result)
         st.write(result["moves"])
 
+# streamlit does not process the chess board visualization, code below is irrelavent
+def show_board():
 
+    print(board)
 def play_chess_game(moves):
     board = chess.Board()
     images = []
 
     for move in moves:
         try:
-            board.push_uci(move)
+            board.push_san(move)
 
             # Convert chessboard to SVG and then to an image
             svg_board = chess.svg.board(board=board)
-            # image = Image.open(BytesIO(chess.svg.board(board=board, size=400, lastmove=chess.Move.from_uci(move))))
-
+            print(svg_board)
             # Append the image to the list
-            images.append(image)
+            #images.append(image)
         except ValueError:
             st.error(f"Invalid move: {move}")
             break
 
     return images
 
-
-moves_input = st.text_input("Enter chess moves (e.g., e4, e5, Nf3):")
-
-moves = [move.strip() for move in moves_input.split(',')]
-images = play_chess_game(moves)
-# Display the images in Streamlit
-for image in images:
-    st.image(image, caption="Chessboard", use_column_width=True)
+board = chess.Board()
+st.write(board)
